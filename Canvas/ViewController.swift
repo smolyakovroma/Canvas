c59f120003740036209f488b0c1d15656905e12c
romansmolakov <smolyakovroma@gmail.com>
2018-11-03 01:29:47 +0700

    Initial Commit

---
+++ Canvas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Canvas
+//
+//  Created by Роман Смоляков on 03/11/2018.
+//  Copyright © 2018 Роман Смоляков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
